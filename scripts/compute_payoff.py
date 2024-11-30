3ba7ba0c6316c2e9ae046edf332b1c0f9be07203
Jorge Augusto Martinez-Ortiz <joy@Jorges-MacBook-Pro.local>
2024-11-30 11:45:35 -0500

    Introduced averaging in payoff

--- scripts/compute_payoff.py
+++ scripts/compute_payoff.py
@@ -1,10 +1,12 @@
-from utils import load_graph, save_dict, save_graph
+from utils import load_graph, save_dict
+from networkx import write_gexf
 from simulate import Contagion
 
 graph = load_graph(snakemake.input[0])
 node_r = int(snakemake.wildcards.i)
 node_b = int(snakemake.wildcards.j)
 n_iterations = snakemake.params.n_iterations
+n_trials = snakemake.params.n_trials
 
 # Instantiate simulation object
 simulation = Contagion(graph,
@@ -13,9 +15,14 @@ simulation = Contagion(graph,
                        n_iterations)
 
 # Run simulation
-simulation.run()
+count = {'red':0, 'blue':0}
+for i in range(n_trials):
+    simulation.run()
+
+    count['red'] += simulation.count['red'] / n_trials
+    count['blue'] += simulation.count['blue'] / n_trials
 
 # Save data
-save_dict(snakemake.output[0],simulation.count)
-save_graph(snakemake.output[1],simulation.graph)
+save_dict(snakemake.output[0],count)
+write_gexf(simulation.graph,snakemake.output[1])
 
