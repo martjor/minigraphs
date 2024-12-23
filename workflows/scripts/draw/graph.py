f9c4fb4428bd5ea62bafa18603ff9799fbf4bb5a
Jorge Martinez-Ortiz <martjor@msu.edu>
2024-12-23 13:43:02 -0500

    ADD: Graph Drawing Miniature Rule

--- workflows/scripts/draw/graph.py
+++ workflows/scripts/draw/graph.py
@@ -1,24 +1,11 @@
-import matplotlib as mpl
-mpl.use('Agg')
 import matplotlib.pyplot as plt 
 import networkx as nx
-from fa2 import ForceAtlas2
-import sys
-from scipy.sparse import load_npz
+from scripts.utils.io import load_graph
+from numpy import load
 
-
-
-# Load the graph
-adjacency_file = sys.argv[1]
-drawing_file = sys.argv[2]
-graph = nx.from_scipy_sparse_matrix(load_npz(adjacency_file))
-
-# Calculate layout using force atlas
-forceatlas2 = ForceAtlas2(gravity=1.0)
-
-positions = forceatlas2.forceatlas2_networkx_layout(graph,
-                                                    pos=None,
-                                                    iterations=100)
+# Load the graph and Layout
+graph = load_graph(snakemake.input[0])
+pos = load(snakemake.input[1],allow_pickle=True)[()]
 
 # Draw Graph
 fig, ax = plt.subplots(figsize=(5,5),dpi=600)
@@ -29,16 +16,16 @@ ax.set_yticks([])
 ax.set_xticks([])
 ax.set_facecolor('black')
 
-nx.draw_networkx_nodes(graph,positions,
+nx.draw_networkx_nodes(graph,pos,
                        node_size=0.1,
                        node_color=color,
                        vmin=0.0,
                        vmax=1.0,
                        alpha=0.7,
                        cmap='cool')
-nx.draw_networkx_edges(graph,positions,
+nx.draw_networkx_edges(graph,pos,
                        edge_color="white",
                        alpha=0.05,
                        width=0.5)
 
-plt.savefig(drawing_file,bbox_inches='tight')
+plt.savefig(snakemake.output[0],bbox_inches='tight')
