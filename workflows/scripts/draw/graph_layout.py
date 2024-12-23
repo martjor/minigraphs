d7e1066e8c2bcd3e47efef33c9c281d3dd656981
Jorge Martinez-Ortiz <martjor@msu.edu>
2024-12-23 12:31:35 -0500

    ADD: Graph Layout Rule

--- workflows/scripts/draw/graph_layout.py
+++ workflows/scripts/draw/graph_layout.py
@@ -1,13 +1,16 @@
 import networkx as nx 
 import yaml
 from numpy import save
-from utils import load_graph
+from scripts.utils.io import load_graph
 
 # Load graph
 graph = load_graph(snakemake.input[0])
 
 # Calculate Layout
-pos = nx.forceatlas2_layout(graph)
+pos = nx.forceatlas2_layout(
+    graph,
+    max_iter=snakemake.params.max_iter
+)
 
 # Save layout
 save(snakemake.output[0],pos)
