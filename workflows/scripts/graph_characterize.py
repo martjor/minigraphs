728733559a60764ecf62b05deecdfb38888198de
Jorge Martinez-Ortiz <martjor@msu.edu>
2025-01-11 20:55:22 -0500

    ADD: Distribution metrics in graph characterization

--- workflows/scripts/graph_characterize.py
+++ workflows/scripts/graph_characterize.py
@@ -3,7 +3,9 @@ import networkx as nx
 from yaml import dump
 from scipy.sparse import load_npz
 from scipy.sparse.linalg import eigs
-import numpy as np
+from numpy import real 
+from scipy.stats import moment
+
 
 def graph_components(G):
     # Get connected components of the graph
@@ -14,30 +16,11 @@ def graph_components(G):
 
     return graphs
 
-def degree_distribution(graph):
-    '''Computes the node degree distribution of a graph
+def degree_sequence(graph):
+    '''Computes the node degree sequence of a graph
     '''
-    hist = nx.degree_histogram(graph)
-    dist = np.zeros((len(hist),2))
-    dist[:,0] = np.arange(len(hist))
-    dist[:,1] = hist
-    
-    # Normalize distribution
-    dist[:,1] /= dist[:,1].sum()
-    
-    return dist 
-
-def moment(dist,p):
-    '''Calculates the pth momenth of a distribution
-    '''
-    moment = ((dist[:,0] ** p) * dist[:,1]).sum()
-    return moment
-
-def moment_central(dist,mean,p):
-    '''Calculates the pth central moment of a distribution
-    '''
-    moment = np.power(np.abs(dist[:,1] * (dist[:,0] - mean)),p).sum()
-    return moment 
+    sequence = sorted((d for n, d in graph.degree()))
+    return sequence 
 
 # PREAMBLE 
 adjacency_file = snakemake.input[0]
@@ -63,16 +46,16 @@ components = graph_components(graph)
 evals,_ = eigs(adjacency,2)
 
 # Calculate degree distribution
-dist = degree_distribution(graph)
+sequence = degree_sequence(graph)
 
 # Evaluate metrics
 metrics = {metric: func(graph) for metric, func in functions.items()}
 metrics['n_components'] = len(components)
 metrics['connectivity'] = components[0].number_of_nodes() / graph.number_of_nodes()
-metrics['eig_1'] = float(np.real(evals[0]))
-metrics['eig_2'] = float(np.real(evals[1]))
-metrics['dd_m1'] = moment(dist,1)
-metrics['dd_m2'] = moment(dist,2)
+metrics['eig_1'] = float(real(evals[0]))
+metrics['eig_2'] = float(real(evals[1]))
+metrics['dd_m1'] = float(moment(sequence,order=1,center=0.0))
+metrics['dd_m2'] = float(moment(sequence,order=2,center=0.0))
 
 # WRITE METRICS
 with open(metrics_file,'w') as file:
