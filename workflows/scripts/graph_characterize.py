28f24c79ade775a5d7e163cc926f61aeacfb5c1c
Jorge Martinez-Ortiz <85181632+martjor@users.noreply.github.com>
2025-01-11 21:45:18 -0500

    Implementation of the degree distribution moments (#14)
    * ADD: Graph distribution characterization
    
    * ADD: Distribution metrics in graph characterization
    
    * RM: Old Graph Metrics Characterization
    
    * FIX: Variable name misuse in dictionary-loading function
    
    * ADD: Include degree distribution visualization

--- workflows/scripts/graph_characterize.py
+++ workflows/scripts/graph_characterize.py
@@ -3,7 +3,9 @@ import networkx as nx
 from yaml import dump
 from scipy.sparse import load_npz
 from scipy.sparse.linalg import eigs
-from numpy import real
+from numpy import real 
+from scipy.stats import moment
+
 
 def graph_components(G):
     # Get connected components of the graph
@@ -13,6 +15,12 @@ def graph_components(G):
     graphs = [G.subgraph(c).copy() for c in components]
 
     return graphs
+
+def degree_sequence(graph):
+    '''Computes the node degree sequence of a graph
+    '''
+    sequence = sorted((d for n, d in graph.degree()))
+    return sequence 
 
 # PREAMBLE 
 adjacency_file = snakemake.input[0]
@@ -37,12 +45,17 @@ components = graph_components(graph)
 # Calculate eigenvalues
 evals,_ = eigs(adjacency,2)
 
+# Calculate degree distribution
+sequence = degree_sequence(graph)
+
 # Evaluate metrics
 metrics = {metric: func(graph) for metric, func in functions.items()}
 metrics['n_components'] = len(components)
 metrics['connectivity'] = components[0].number_of_nodes() / graph.number_of_nodes()
 metrics['eig_1'] = float(real(evals[0]))
 metrics['eig_2'] = float(real(evals[1]))
+metrics['dd_m1'] = float(moment(sequence,order=1,center=0.0))
+metrics['dd_m2'] = float(moment(sequence,order=2,center=0.0))
 
 # WRITE METRICS
 with open(metrics_file,'w') as file:
