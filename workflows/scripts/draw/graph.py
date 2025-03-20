8804876ec83d142ed55ca1d02b02bc5e8e2cd283
Jorge Martinez-Ortiz <martjor@msu.edu>
2025-03-20 14:48:34 -0400

    UP: Graph visualization of miniatures

--- workflows/scripts/draw/graph.py
+++ workflows/scripts/draw/graph.py
@@ -1,31 +1,20 @@
-import matplotlib.pyplot as plt 
-import networkx as nx
-from scripts.utils.io import load_graph
 from numpy import load
+import matplotlib.pyplot as plt
+from scripts.utils.io import load_graph, load_dict
+import networkx as nx
+from module import draw_graph
 
-# Load the graph and Layout
+# Load graph and layout
 graph = load_graph(snakemake.input[0])
-pos = load(snakemake.input[1],allow_pickle=True)[()]
+layout = load(snakemake.input[1],allow_pickle=True)[()]
 
-# Draw Graph
-fig, ax = plt.subplots(figsize=(5,5),dpi=600)
-color = list(nx.clustering(graph).values())
+# Draw graph with specified parameters
+fig, ax = plt.subplots(figsize=(10,10), dpi=300)
 
-ax.set_title(f"|V|={graph.number_of_nodes()}, den={nx.density(graph):.2e}")
-ax.set_yticks([])
-ax.set_xticks([])
-ax.set_facecolor('black')
+draw_graph(
+    graph,
+    layout,
+    snakemake.params.properties
+)
 
-nx.draw_networkx_nodes(graph,pos,
-                       node_size=0.1,
-                       node_color=color,
-                       vmin=0.0,
-                       vmax=1.0,
-                       alpha=0.7,
-                       cmap='cool')
-nx.draw_networkx_edges(graph,pos,
-                       edge_color="white",
-                       alpha=0.05,
-                       width=0.5)
-
-plt.savefig(snakemake.output[0],bbox_inches='tight')
+plt.savefig(snakemake.output[0])
