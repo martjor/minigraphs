8fdcb046e185bd8486d7502576bc1748cbaae3ae
Joy <martjor@msu.edu>
2025-05-15 15:32:56 -0400

    Initial chain testing workflow

---
+++ workflows/scripts/test_chain.py
@@ -0,0 +1,27 @@
+from mpi4py.MPI import COMM_WORLD
+from minigraphs.mcmc import ParallelTempering
+from minigraphs.mcmc.chains import SubgraphBoundary
+from minigraphs.metrics import graph_spectrum
+import networkx as nx 
+from minigraphs.data import load_graph
+import sys
+
+hamsterster = load_graph('hamsterster')
+inverse_temperatures = [10, 100, 300, 500, 700, 1000]
+n_nodes = 100
+
+def spectral_radius(graph: nx.Graph) -> float:
+    """Calculates the spectral radius of a graph."""
+    return graph_spectrum(graph)[0]
+
+runner = ParallelTempering(
+    COMM_WORLD,
+    annealer_data=[(SubgraphBoundary(hamsterster, n_nodes, 1), inv_temp) for inv_temp in inverse_temperatures],
+    energy=lambda graph: -spectral_radius(graph),
+    exchange_freq=100,
+    n_steps=10000,
+)
+
+runner.run()
+runner.gather_results()
+runner.results_to_csv(sys.argv[1])
