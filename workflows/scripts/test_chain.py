f90738fa541312f5582d8bd77ba26871d1ee4be9
Joy <martjor@msu.edu>
2025-05-16 18:04:17 -0400

    ADD: implement testing for coarsening chain

--- workflows/scripts/test_chain.py
+++ workflows/scripts/test_chain.py
@@ -1,6 +1,6 @@
 from mpi4py.MPI import COMM_WORLD
 from minigraphs.mcmc import ParallelTempering
-from minigraphs.mcmc.chains import SubgraphBoundary
+from minigraphs.mcmc.chains import CoarseningChain
 from minigraphs.metrics import graph_spectrum
 import networkx as nx 
 from minigraphs.data import load_graph
@@ -10,14 +10,10 @@ hamsterster = load_graph('hamsterster')
 inverse_temperatures = [10, 100, 300, 500, 700, 1000]
 n_nodes = 100
 
-def spectral_radius(graph: nx.Graph) -> float:
-    """Calculates the spectral radius of a graph."""
-    return graph_spectrum(graph)[0]
-
 runner = ParallelTempering(
     COMM_WORLD,
-    annealer_data=[(SubgraphBoundary(hamsterster, n_nodes, int(sys.argv[1])), inv_temp) for inv_temp in inverse_temperatures],
-    energy=lambda graph: -spectral_radius(graph),
+    annealer_data=[(CoarseningChain(hamsterster, 200), inv_temp) for inv_temp in inverse_temperatures],
+    energy=nx.average_clustering,
     exchange_freq=100,
     n_steps=10000,
 )
